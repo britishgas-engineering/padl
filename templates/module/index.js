8b4cf3c3279d749fe7324223c4edf798a98f8c0e
Jonathan Holt <jonathan.a.holt@gmail.com>
2019-03-12 16:20:28 +0000

    fix ie issue

--- templates/module/index.js
+++ templates/module/index.js
@@ -2,7 +2,7 @@
 
   _INSERT_ES5_ADAPTER_
 
-  const getRoot = () => {
+  function getRoot() {
     const scripts = document.getElementsByTagName('script');
     const lastScript = scripts[scripts.length - 1].src;
     const list = lastScript.match(new RegExp('https?://[^/]*'))
@@ -10,7 +10,7 @@
     return list && list[0] || '';
   };
 
-  const root = `${getRoot()}/`;
+  const root = getRoot() + "/";
 
   window.WebComponents = window.WebComponents || {};
   window.WebComponents.root = root;
